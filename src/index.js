c43fcf9c15857fe236918a02e8363ca4cc296b0a
Shibin Lal <shilal@publicisgroupe.net>
2020-07-26 12:08:07 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+document.title = "Shibn_Inthad"
+ReactDOM.render(
+	<React.Fragment>
+		<App />
+	</React.Fragment>,
+	document.getElementById("root")
+);
